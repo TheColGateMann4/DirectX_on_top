c5a18dcd285eb81ddb50a6be6baa2613727f81ec
TheColGateMann <TheColGateMann4@gmail.com>
2023-10-03 01:32:37 +0200

    anisotropic filtering

--- SamplerState.cpp
+++ SamplerState.cpp
@@ -6,7 +6,8 @@ SamplerState::SamplerState(GFX& gfx, D3D11_TEXTURE_ADDRESS_MODE mode)
 	HRESULT hr;
 
 	D3D11_SAMPLER_DESC samplerDesc = {};
-	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
+	samplerDesc.MaxAnisotropy = D3D11_REQ_MAXANISOTROPY; // x16
 	samplerDesc.AddressU = mode;
 	samplerDesc.AddressV = mode;
 	samplerDesc.AddressW = mode;
