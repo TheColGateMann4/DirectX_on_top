6fc4531085d2a59cd54c9d3229d1315f87dba914
TheColGateMann <TheColGateMann4@gmail.com>
2024-05-12 03:15:41 +0200

    + Took constants to class body which makes code more readable and logical

--- FPSCounter.cpp
+++ FPSCounter.cpp
@@ -16,7 +16,7 @@ void FPSCounter::Draw(const float deltaTime)
 	m_accumulatedDeltatime += deltaTime;
 	m_countedTimes++;
 
-	if (m_accumulatedDeltatime >= 0.2f)
+	if (m_accumulatedDeltatime >= m_fpsCounterDelay)
 	{
 		float tempFps = 1.0f / (m_accumulatedDeltatime / m_countedTimes);
 
@@ -35,25 +35,23 @@ void FPSCounter::Draw(const float deltaTime)
 		ImGuiWindowFlags_NoInputs;
 
 	{
-		constexpr float offsetFromCorner = 2.0f;
-
 		ImGui::PushStyleColor(ImGuiCol_Border, 0);
 		ImGui::PushStyleColor(ImGuiCol_WindowBg, 0);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0.0f, offsetFromCorner });
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0.0f, m_offsetFromCorner });
 
 		if (ImGui::Begin("FpsCounter", nullptr, windowFlags))
 		{
 			std::string fpsText = std::to_string(fpsNumber);
 
 			ImVec2 textWidth = ImGui::CalcTextSize(fpsText.c_str());
-			float newWindowWidth = textWidth.x + offsetFromCorner * 10;
+			float newWindowWidth = textWidth.x + m_offsetFromCorner * 10;
 
 			ImGui::SetWindowSize({ newWindowWidth, textWidth.y });
 			ImGui::SetWindowPos({ m_screenWidth - newWindowWidth, 0.0f });
 
 
 			if (textWidth.x < newWindowWidth)
-				ImGui::SetCursorPosX(newWindowWidth - (textWidth.x + offsetFromCorner * 2));
+				ImGui::SetCursorPosX(newWindowWidth - (textWidth.x + m_offsetFromCorner * 2));
 
 
 			ImGui::Text(fpsText.c_str());
--- FPSCounter.h
+++ FPSCounter.h
@@ -14,5 +14,8 @@ private:
 
 	int m_countedTimes = 0;
 	float m_accumulatedDeltatime = 0.0f;
+
+	static constexpr float m_fpsCounterDelay = 0.2f;
+	static constexpr float m_offsetFromCorner = 2.0f;
 };
 
