5db607dab85c62eb4f1b586bcfef7c1b76cc1110
TheColGateMann <TheColGateMann4@gmail.com>
2024-06-30 02:26:26 +0200

    +Now checking for objects that corners of bounds aren't visible +Made optimisation that will skip this calculations if object is already marked as visible

--- CS_FrustumChecking.hlsl
+++ CS_FrustumChecking.hlsl
@@ -59,7 +59,7 @@ float3 GetFrustumVertice(uint index)
     return float3(cameraFrustumData[index], cameraFrustumData[index + 1], cameraFrustumData[index + 2]);
 }
 
-// Checks if the ourPoint is between two points on slopes
+// Gets values on both slopes on level of ourPoint
 // ! secondPoint has to be larger than firstPoint in the slope axis
 // ! pointOnOtherSide is point going into - of axis
 //
@@ -67,8 +67,8 @@ float3 GetFrustumVertice(uint index)
 //
 //                                      ------* secondPoint
 //                               ---X---
-//                        -------   |  // we basicly get Y value where the point hit line
-//        firstPoint *----          |  // between firstPoint and Second Point
+//                        -------   |
+//        firstPoint *----          |
 //                   |              |
 //               /   |              |
 //              /    |              |
@@ -82,23 +82,63 @@ float3 GetFrustumVertice(uint index)
 //                                      -------
 //
 //      *  represents point thats being taken as argument
-//      X  represents point calculated, we use these to check if ourPoint is in wanted area
+//      X  represents the values that will be calculated, topPoint and bottomPoint
 //
-bool CheckForSlope(float3 firstPoint, float3 secondPoint, float3 pointOnOtherSide, float3 ourPoint, bool getY)
+void GetSlopeValues(float3 firstPoint, float3 secondPoint, float pointOnOtherSide, float3 ourPoint, bool getY, out float topPoint, out float bottomPoint)
 {
-    float ourPointWantedAxis = getY ? ourPoint.y : ourPoint.x;    
     float firstPointWantedAxis = getY ? firstPoint.y : firstPoint.x;    
     float secondPointWantedAxis = getY ? secondPoint.y : secondPoint.x;
-    float pointOnOtherSideWantedAxis = getY ? pointOnOtherSide.y : pointOnOtherSide.x;
     
-    float difference = (abs(ourPoint.z) - abs(firstPoint.z)) / (abs(secondPoint.z) - abs(firstPoint.z));
+    float difference = (ourPoint.z - firstPoint.z) / (secondPoint.z - firstPoint.z);
 
     float offsetFromPoint = (secondPointWantedAxis - firstPointWantedAxis) * difference;
 
-    float topPoint = firstPointWantedAxis + offsetFromPoint;
-    float bottomPoint = pointOnOtherSideWantedAxis - offsetFromPoint;
+    topPoint = firstPointWantedAxis + offsetFromPoint;
+    bottomPoint = pointOnOtherSide - offsetFromPoint;
+}
+
+// Checks if our point is in between sloped 
+bool CheckForSlope(float3 firstPoint, float3 secondPoint, float pointOnOtherSide, float3 ourPoint, bool getY)
+{
+    float ourPointWantedAxis = getY ? ourPoint.y : ourPoint.x;
+    float topPoint, bottomPoint;
+
+    GetSlopeValues(firstPoint, secondPoint, pointOnOtherSide, ourPoint, getY, topPoint, bottomPoint);
 
     return ourPointWantedAxis < topPoint && ourPointWantedAxis > bottomPoint;
+}
+
+bool CheckIfObjectsAxisOverlapsWithFrustum(float cubePlusVerticePosAxis, float cubeMinusVerticePosAxis, float frustumVerticePosAxis, float farZFrustumVerticeAxis, float nearZFrustumVerticeAxis)
+{
+    // "first test" checks if the cube vertices are in this scheme:
+    //
+    //      *                 *                 *
+    // cubeVertice      FrustumVertice     cubeVertice
+
+    // "second test" checks if the cube vertices are in frustum range, for example Z axis
+    //  View frustum Z-axis from camera: 
+    //
+    //      [Camera] nearZ<------------------------------------------->FarZ
+    //                                  *
+    //                      point in correct position
+
+    // normal first test
+    if(cubePlusVerticePosAxis > frustumVerticePosAxis && frustumVerticePosAxis > cubeMinusVerticePosAxis)
+        return true;
+
+    // reversed first test
+    if(cubePlusVerticePosAxis < frustumVerticePosAxis && frustumVerticePosAxis < cubeMinusVerticePosAxis)
+        return true;
+
+    // normal second test
+    if(cubePlusVerticePosAxis > nearZFrustumVerticeAxis && farZFrustumVerticeAxis > cubeMinusVerticePosAxis)
+        return true;
+
+    // reversed second test
+    if(cubePlusVerticePosAxis < nearZFrustumVerticeAxis && farZFrustumVerticeAxis < cubeMinusVerticePosAxis)
+        return true;
+
+    return false;
 }
 
 // when whole plan gets finished, this will be called:
@@ -109,11 +149,12 @@ bool CheckForSlope(float3 firstPoint, float3 secondPoint, float3 pointOnOtherSid
 [numthreads(1, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
-
     for(int iModelID = 0; iModelID < numberOfModels; iModelID++)
     {
         if(modelValidity[iModelID] == 0)
             continue;
+
+        bool passedDirectVisibleCheck = false;
 
         //checking for vertices placed inside frustum
         for(int iCubeVertice = 0; iCubeVertice < 8; iCubeVertice++)
@@ -125,13 +166,73 @@ void main( uint3 DTid : SV_DispatchThreadID )
             if(verticePos.z < nearZFrustumVertice.z || verticePos.z > farZFrustumVertice.z)
                 continue;
 
-            if(!CheckForSlope(nearZFrustumVertice, farZFrustumVertice, GetFrustumVertice(1), verticePos, true))
+            if(!CheckForSlope(nearZFrustumVertice, farZFrustumVertice, GetFrustumVertice(1).y, verticePos, true))
                 continue;
 
-            if(!CheckForSlope(nearZFrustumVertice, farZFrustumVertice, GetFrustumVertice(3), verticePos, false))
+            if(!CheckForSlope(nearZFrustumVertice, farZFrustumVertice, GetFrustumVertice(3).x, verticePos, false))
                 continue;
 
             result[iModelID] = 1;
+            passedDirectVisibleCheck = true;
+            break;
+        }
+
+        // if object is already marked as visible we don't need to check it in more complicated ways
+        if(passedDirectVisibleCheck)
+            continue;
+
+
+        //checking for vertices that are around frustum, and we don't see them, but their middle part can be seen
+        float3 cubeMinusVerticePos = GetCubeVertice(0, iModelID);
+        float3 cubePlusVerticePos = GetCubeVertice(6, iModelID);
+
+        float3 nearZFrustumVertice = GetFrustumVertice(0);
+        float3 farZFrustumVertice = GetFrustumVertice(4);
+
+        for(int iFrustumVertice = 0; iFrustumVertice < 8; iFrustumVertice++)
+        {
+            float3 frustumVerticePos = GetFrustumVertice(iFrustumVertice);
+        
+            // the point of this is, when object is infront of camera it has to be on at least two axels to be in frustum, thats why its okay if its not on one axis
+            bool failedOnce = false;
+                
+
+            // checking Z axis
+            {
+                if(!CheckIfObjectsAxisOverlapsWithFrustum(cubePlusVerticePos.z, cubeMinusVerticePos.z, frustumVerticePos.z, farZFrustumVertice.z, nearZFrustumVertice.z))
+                    failedOnce = true;
+            }
+            
+
+            // top and bottom point on current slope at given Z depth
+            float topPoint, bottomPoint;
+
+
+            // checking X axis, we need to account for slope on view frustum
+            {                    
+                GetSlopeValues(nearZFrustumVertice, farZFrustumVertice, GetFrustumVertice(3).y, cubePlusVerticePos, false, topPoint, bottomPoint);
+                
+                if(!CheckIfObjectsAxisOverlapsWithFrustum(cubePlusVerticePos.x, cubeMinusVerticePos.x, frustumVerticePos.x, topPoint, bottomPoint))
+                    if(failedOnce)
+                    {
+                        continue;
+                    }
+                    else
+                        failedOnce = true;
+            }
+          
+
+            // checking Y axis, we need to account for slope on view frustum as well
+            {
+                GetSlopeValues(nearZFrustumVertice, farZFrustumVertice, GetFrustumVertice(1).x, cubePlusVerticePos, true, topPoint, bottomPoint);
+
+                if(!CheckIfObjectsAxisOverlapsWithFrustum(cubePlusVerticePos.y, cubeMinusVerticePos.y, frustumVerticePos.y, topPoint, bottomPoint))
+                    if(failedOnce)
+                        continue;
+            }
+
+            result[iModelID] = 1;
+            break;
         }
     }
 }
